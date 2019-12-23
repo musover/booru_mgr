83f47788b48a087b9e92019343a88902ac46eb53
maelask3 <meoberto@mthree.es>
2019-12-23 18:05:23 +0100

    Building with maven, and also sonarqube intergration

--- src/main/java/pers/net/Danbooru2.java
+++ src/main/java/pers/net/Danbooru2.java
@@ -6,14 +6,12 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import dom.datatype.Artist;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.*;
